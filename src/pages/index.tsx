2b2804acf5b560257f20e747f41a74eefb30e884
Jess Medeiros <jessicamedeirosp96@yahoo.com.br>
2023-07-19 14:25:53 -0300

    limpando arquivos e adicionando site em construção

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="underConstruction content-flex content-flex-center">
+      <h1>Site em construção!!</h1>
+    </div>
+  );
+};
+
+export default Home;
