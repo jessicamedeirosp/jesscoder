5ed0b8601e617ef669cf6a0c4d96879a91e16f30
Jess Medeiros <jessicamedeirosp96@yahoo.com.br>
2023-07-24 17:24:59 -0300

    ajustando client prismic

--- services/prismic.ts
+++ services/prismic.ts
@@ -1,10 +1,7 @@
 import Prismic from "@prismicio/client";
 
 export function getPrismicClient(req?: unknown) {
-  const endpoint = process.env.PRISMIC_ENDPOINT
-    ? process.env.PRISMIC_ENDPOINT
-    : "";
-  const prismic = Prismic.client(endpoint, {
+  const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT as string, {
     req,
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
   });
